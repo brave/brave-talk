bec63ffc71bbc660401d56074875efe3e3bdf43c
Hadi Amjad <hadiyoville25@gmail.com>
2022-05-18 16:41:02 -0400

    counter

--- src/general-store.ts
+++ src/general-store.ts
@@ -1,8 +1,17 @@
-const PROMO_STORAGE_KEY = "extension_promo_dismissed";
+const PROMO_STORAGE_KEY = "1";
 
 export function shouldShowExtensionPromo(): boolean {
   const value = window.localStorage.getItem(PROMO_STORAGE_KEY);
   return value !== "true";
+}
+
+export function incrementExtensionPromoCounter(): boolean {
+  var value = Number(window.localStorage.getItem(PROMO_STORAGE_KEY));
+  if (value !== 3) {
+    value += 1;
+    window.localStorage.setItem(PROMO_STORAGE_KEY, value.toString());
+  }
+  return value == 3;
 }
 
 export function recordExtensionPromoDismissed(): void {
