3f063c9b61bb50bcae170c9072f8e53c4a7f72bf
Graham Tackley <gtackley@brave.com>
2023-02-21 10:58:56 +0000

    Show message if WebRTC unavailable

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -9,6 +9,7 @@ import { SubscriptionCTA } from "./SubscriptionCTA";
 import { DownloadBrave } from "./DownloadBrave";
 import React from "react";
 import { Recordings } from "./Recordings";
+import { SectionWithLogo } from "./SectionWithLogo";
 
 interface Props {
   onStartCall: DispatchWithoutAction;
@@ -26,31 +27,42 @@ export const WelcomeScreen: React.FC<Props> = ({
   const browserProps = useBrowserProperties();
   const subscribed = useSubscribedStatus();
 
-  const shouldShowDownloadBravePrompt =
-    !hasInitialRoomName && browserProps && !browserProps.isBrave;
+  const Body: React.FC = () => {
+    if (!hasInitialRoomName && browserProps && !browserProps.isBrave) {
+      return <DownloadBrave />;
+    }
+
+    if (browserProps && !browserProps.supportsWebRTC) {
+      return (
+        <SectionWithLogo
+          heading="Brave Talk"
+          subhead="Your iOS device appears to have Lockdown Mode enabled, which prevents Brave Talk from working."
+        />
+      );
+    }
+    return (
+      <React.Fragment>
+        <JoinCallSection
+          subscribed={subscribed}
+          browser={browserProps}
+          onStartCall={onStartCall}
+          notice={notice}
+          disabled={disabled}
+          hideButtons={hasInitialRoomName}
+        />
+
+        <Recordings />
+
+        {!hasInitialRoomName && <SubscriptionCTA subscribed={subscribed} />}
+      </React.Fragment>
+    );
+  };
 
   return (
     <Background>
       <Header subscribed={subscribed} />
       <div css={{ flexGrow: 1, padding: "0 12px" }}>
-        {shouldShowDownloadBravePrompt ? (
-          <DownloadBrave />
-        ) : (
-          <React.Fragment>
-            <JoinCallSection
-              subscribed={subscribed}
-              browser={browserProps}
-              onStartCall={onStartCall}
-              notice={notice}
-              disabled={disabled}
-              hideButtons={hasInitialRoomName}
-            />
-
-            <Recordings />
-
-            {!hasInitialRoomName && <SubscriptionCTA subscribed={subscribed} />}
-          </React.Fragment>
-        )}
+        <Body />
       </div>
       <Footer />
     </Background>
