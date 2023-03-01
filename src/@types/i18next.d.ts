81d382953893209bc80afa2fcf8ff9a73822f5f1
Graham Tackley <gtackley@brave.com>
2023-03-01 17:29:41 +0000

    Additional explanitory comment

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,5 +1,8 @@
-// import the original type declarations
 import { resources, defaultNS } from "../i18n/i18next";
+
+// see https://www.i18next.com/overview/typescript
+// for why this is set up like this - it gives us typesafety, confirming
+// at compile time that the translations keys we reference exist.
 
 declare module "i18next" {
   interface CustomTypeOptions {
