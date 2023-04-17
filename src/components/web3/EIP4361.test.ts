0625afbf87a327dae91362a1425c26739f4c7397
John Halbert <jhalbert@brave.com>
2023-04-17 15:07:09 -0500

    Remove unused parse method

--- src/components/web3/EIP4361.test.ts
+++ src/components/web3/EIP4361.test.ts
@@ -1,4 +1,4 @@
-import { parseEIP4361Message, createEIP4361Message } from "./EIP4361";
+import { createEIP4361Message } from "./EIP4361";
 
 it("Should create an EIP4361 message in the correct format", () => {
   const now = new Date().toISOString();
@@ -39,49 +39,6 @@ Resources:
     ],
   };
   const actual = createEIP4361Message(message);
-
-  expect(actual).toEqual(expected);
-});
-
-it("Should parse an EIP4361 message correctly", () => {
-  const now = new Date().toISOString();
-  const requestId = "1";
-  const message = `talk.brave.com wants you to sign in with your Ethereum account:
-0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
-
-I accept the ServiceOrg Terms of Service: https://service.invalid/tos
-
-URI: https://talk.brave.com
-Version: 1
-Chain ID: 1
-Nonce: 32891756
-Issued At: ${now}
-Expiration Time: ${now}
-Not Before: ${now}
-Request ID: ${requestId}
-Resources:
-- ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/
-- https://example.com/my-web2-claim.json`;
-
-  const expected = {
-    domain: "talk.brave.com",
-    address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    statement:
-      "I accept the ServiceOrg Terms of Service: https://service.invalid/tos",
-    uri: "https://talk.brave.com",
-    version: "1",
-    chainId: 1,
-    nonce: "32891756",
-    issuedAt: now,
-    expirationTime: now,
-    notBefore: now,
-    requestId: requestId,
-    resources: [
-      "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/",
-      "https://example.com/my-web2-claim.json",
-    ],
-  };
-  const actual = parseEIP4361Message(message);
 
   expect(actual).toEqual(expected);
 });
