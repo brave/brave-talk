38159d79546f64648ef7f563a396e221b6ab06e9
John Halbert <jhalbert@brave.com>
2023-04-20 03:29:12 -0500

    Fixes #819

--- src/components/web3/Login.tsx
+++ src/components/web3/Login.tsx
@@ -7,19 +7,32 @@ interface Props {
   onAddressSelected: Dispatch<string>;
 }
 export const Login: React.FC<Props> = ({ web3address, onAddressSelected }) => {
-  const [notice, setNotice] = useState("");
+  const [notice, setNotice] = useState<JSX.Element | undefined>();
 
   useEffect(() => {
     if (!web3address) {
-      setNotice("Requesting wallet access...");
+      setNotice(<span>Requesting wallet access...</span>);
       web3Login()
         .then((address) => {
           onAddressSelected(address);
-          setNotice("");
+          setNotice(undefined);
         })
         .catch((err) => {
           console.error("!!! web3 window.ethereum.request", err);
-          setNotice("Failed to connect to wallet");
+          setNotice(
+            <div>
+              <p>
+                Failed to connect to wallet. Please reload the page and try
+                again.
+              </p>
+              <a
+                href="https://support.brave.com/hc/en-us/articles/4415497656461-Brave-Wallet-FAQ"
+                style={{ color: "inherit" }}
+              >
+                Help with Brave Wallet
+              </a>
+            </div>
+          );
         });
     }
   }, [web3address, onAddressSelected]);
