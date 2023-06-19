af3e579d9d35779c832515bc1859d7486bc937ed
hadiamjad <hadiyoville25@gmail.com>
2023-06-19 11:18:38 -0400

    solana nft gating poap blocked for Solana

--- webpack.config.js
+++ webpack.config.js
@@ -109,7 +109,7 @@ module.exports = {
     proxy: {
       "/api": {
         target: "https://subscriptions.bsg.brave.software",
-        target: "http://localhost:3003/",
+        // target: "http://localhost:3003/",
         pathRewrite: { "^/api": "" },
         changeOrigin: true,
       },
