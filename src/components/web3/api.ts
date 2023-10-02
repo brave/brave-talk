a9724a04e56595fd4f67404021b4d0aa12e38c01
Marshall T. Rose <mrose17@gmail.com>
2023-10-02 11:17:16 -0400

    `spam_score` sorting

--- src/components/web3/api.ts
+++ src/components/web3/api.ts
@@ -102,7 +102,7 @@ export const web3LoginSol = async (): Promise<string> => {
 
 export const web3NFTs = async (address: string): Promise<NFT[]> => {
   try {
-    const getNFTsURL = `${SIMPLEHASH_PROXY_ROOT_URL}/api/v0/nfts/owners?chains=ethereum,solana,polygon&wallet_addresses=${encodeURIComponent(
+    const getNFTsURL = `${SIMPLEHASH_PROXY_ROOT_URL}/api/v0/nfts/owners_v2?chains=ethereum,solana,polygon&order_by=spam_score_asc&wallet_addresses=${encodeURIComponent(
       address
     )}`;
     console.log(`>>> GET ${getNFTsURL}`);
@@ -202,7 +202,7 @@ export const web3NFTcollections = async (
   };
 
   try {
-    const getNFTsByWalletURL = `${SIMPLEHASH_PROXY_ROOT_URL}/api/v0/nfts/owners?chains=ethereum,solana,polygon&wallet_addresses=${encodeURIComponent(
+    const getNFTsByWalletURL = `${SIMPLEHASH_PROXY_ROOT_URL}/api/v0/nfts/owners_v2?chains=ethereum,solana,polygon&order_by=spam_score_asc&wallet_addresses=${encodeURIComponent(
       address
     )}`;
     console.log(`>>> GET ${getNFTsByWalletURL}`);
