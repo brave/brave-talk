0be65b5b46b1948b24895dd3f5a6337f8452d56c
Marshall T. Rose <mrose17@gmail.com>
2023-11-14 07:20:55 -0500

    Fixes #1290 which updates prettier, but doesn't run prettier...
    Looks good on desktop, `dev1`, and `dev3`

--- src/environment.ts
+++ src/environment.ts
@@ -4,10 +4,10 @@ export const config = {
   vpaas: isDevelopment
     ? "vpaas-magic-cookie-cd4131ef77674a71b73411408226e232"
     : env === "staging"
-    ? "vpaas-magic-cookie-520aa9362071418c8a8661950bc0a470"
-    : env === "local"
-    ? "vpaas-magic-cookie-cd4131ef77674a71b73411408226e232"
-    : "vpaas-magic-cookie-a4818bd762a044998d717b70ac734cfe",
+      ? "vpaas-magic-cookie-520aa9362071418c8a8661950bc0a470"
+      : env === "local"
+        ? "vpaas-magic-cookie-cd4131ef77674a71b73411408226e232"
+        : "vpaas-magic-cookie-a4818bd762a044998d717b70ac734cfe",
   webrtc_domain: "8x8.vc",
 };
 
