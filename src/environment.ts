d1edcfc7381a4a2ad113691bde59487800ecf792
Marshall T. Rose <mrose17@gmail.com>
2024-01-19 11:44:33 -0500

    set webrtc_domain based on isDevelopment
    if `isDevelopment` use the the JAAS staging server

--- src/environment.ts
+++ src/environment.ts
@@ -8,7 +8,7 @@ export const config = {
       : env === "local"
         ? "vpaas-magic-cookie-cd4131ef77674a71b73411408226e232"
         : "vpaas-magic-cookie-a4818bd762a044998d717b70ac734cfe",
-  webrtc_domain: "8x8.vc",
+  webrtc_domain: isDevelopment ? "stage.8x8.vc" : "8x8.vc",
 };
 
 export const isProduction = env === "production";
