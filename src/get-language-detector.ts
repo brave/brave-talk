cdefbb4d931bd0cfd9ed4782842753f237ec70ab
bcaller <bcaller@brave.com>
2024-06-05 15:30:23 +0900

    日本語が出来ますか？

--- src/get-language-detector.ts
+++ src/get-language-detector.ts
@@ -2,6 +2,6 @@ export function getLangPref(): string {
   const language =
     (navigator.languages && navigator.languages[0]) || navigator.language;
 
-  if (language === "ja") return "ja";
+  if (language === "ja-JP" || language === "ja") return "ja";
   return "en";
 }
