6a7cb1f1513c74c39757950f75208a8646d4127d
Marshall T. Rose <mrose17@gmail.com>
2025-11-01 14:33:42 -0400

    Move dev3 back to development

--- src/jitsi/options.ts
+++ src/jitsi/options.ts
@@ -187,7 +187,7 @@ export const jitsiOptions = (
       // remove 'dial-in'
       SHARING_FEATURES: ["email", "url", "embed"],
       SHOW_CHROME_EXTENSION_BANNER: false,
-      SUPPORT_URL: "https://community.brave.com/",
+      SUPPORT_URL: "https://community.brave.app/",
       TOOLBAR_BUTTONS: [
         "microphone",
         "camera",
@@ -248,6 +248,17 @@ export const jitsiOptions = (
         );
         options.configOverwrite.buttonsWithNotifyClick.push("leo");
         options.configOverwrite.mainToolbarButtons = [
+          [
+            "microphone",
+            "camera",
+            "desktop",
+            "chat",
+            "raisehand",
+            "reactions",
+            "participants-pane",
+            "leo",
+            "tileview",
+          ],
           [
             "microphone",
             "camera",
