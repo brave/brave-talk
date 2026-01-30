bf602f97a9a4ded10164ed1607b6b13839105f0a
Marshall T. Rose <mrose17@gmail.com>
2026-01-30 13:38:06 -0500

    Create eslint.config.js
    naturally

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+const { defineConfig, globalIgnores } = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+
+const { fixupConfigRules } = require("@eslint/compat");
+
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      parser: tsParser,
+
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    extends: fixupConfigRules(
+      compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+        "prettier",
+      ),
+    ),
+
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+
+      "react/no-unknown-property": [
+        "error",
+        {
+          ignore: ["css"],
+        },
+      ],
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  globalIgnores(["src/js/jwt-decode.js", "**/webpack.config.js"]),
+]);
