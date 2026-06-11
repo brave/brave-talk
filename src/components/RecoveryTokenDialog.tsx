6610d96187a36a3c35575bce91ed00779c0901e7
Marshall T. Rose <mrose17@gmail.com>
2026-06-11 17:35:02 -0400

    Test #1770 on dev2
    Looks good on desktop and `dev1`.

--- src/components/RecoveryTokenDialog.tsx
+++ src/components/RecoveryTokenDialog.tsx
@@ -21,6 +21,7 @@ interface RecoveryTokenDialogProps {
   isOpen: boolean;
   onClose: () => void;
   initialToken: string | null;
+  hasConfabs: boolean;
 }
 
 const bodyStyles = css`
@@ -75,6 +76,7 @@ export default function RecoveryTokenDialog({
   isOpen,
   onClose,
   initialToken,
+  hasConfabs,
 }: RecoveryTokenDialogProps) {
   const { t } = useTranslation();
   const [value, setValue] = useState(initialToken ?? "");
@@ -156,7 +158,11 @@ export default function RecoveryTokenDialog({
       <div css={bodyStyles}>
         <Textarea
           mode="outline"
-          placeholder={t("recovery_token_dialog_textarea_placeholder")}
+          placeholder={t(
+            hasConfabs
+              ? "recovery_token_dialog_textarea_placeholder"
+              : "recovery_token_dialog_textarea_placeholder_no_tokens",
+          )}
           value={value}
           css={{ wordBreak: "break-all" }}
           onInput={({ value }) => {
@@ -206,7 +212,13 @@ export default function RecoveryTokenDialog({
         )}
 
         <p css={descriptionStyles}>
-          <Trans i18nKey="recovery_token_dialog_description" />{" "}
+          <Trans
+            i18nKey={
+              hasConfabs
+                ? "recovery_token_dialog_description"
+                : "recovery_token_dialog_description_no_tokens"
+            }
+          />{" "}
           <a
             href={RECOVERY_TOKEN_LEARN_MORE_URL}
             target="_blank"
@@ -226,14 +238,16 @@ export default function RecoveryTokenDialog({
         >
           {t("recovery_token_dialog_load_button")}
         </Button>
-        <Button
-          kind="filled"
-          onClick={handleGenerate}
-          isLoading={isGenerating}
-          isDisabled={isRecovering}
-        >
-          {t("recovery_token_dialog_generate_button")}
-        </Button>
+        {hasConfabs && (
+          <Button
+            kind="filled"
+            onClick={handleGenerate}
+            isLoading={isGenerating}
+            isDisabled={isRecovering}
+          >
+            {t("recovery_token_dialog_generate_button")}
+          </Button>
+        )}
       </div>
     </Dialog>
   );
